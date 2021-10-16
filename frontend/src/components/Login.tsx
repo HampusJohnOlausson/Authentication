ad4fcb6b49a3c93d711b5911bcc029e8c772e3fa
hampus <hampus.john.olausson@gmail.com>
2021-10-16 19:28:13 +0200

    adjustemnt in login

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
 
 
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('')
+
+   async function getUserData(){
+       let response = await fetch({
+           method: "GET",
+           url: 'http://localhost:5000/Login',
+       });
+       
+       return response.json
+   }
     
 
     return (
         <div>
             <h3>Login</h3>
             <form>
-            <input type="text" placeholder="email"/>
-            <input type="password" placeholder="password"/>
+            <input type="text" placeholder="email" onChange={e => setUserName(e.target.value)}/>
+            <input type="password" placeholder="password" onChange={e => setPassword(e.target.value)}/>
             <button>Sign in</button>
             </form>
         </div>
