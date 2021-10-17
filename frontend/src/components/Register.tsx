3e508e23c5da771339df8ad22932d18d0e6b6ba0
hampus <hampus.john.olausson@gmail.com>
2021-10-17 12:47:31 +0200

    Register user completed

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, { SyntheticEvent, useState } from 'react'
+import { Redirect } from 'react-router'
 
 const Register = () => {
+
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [redirect, setRedirect] = useState(false);
+
+    const submit = async  (e: SyntheticEvent) => {
+        e.preventDefault();
+
+        await fetch('http://localhost:8000/api/register',{
+            method: "POST",
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            }),
+        });
+
+        setRedirect(true);
+    } 
+
+    if(redirect){
+        return <Redirect to="/Login"/>;
+    }
+
     return (
         <div>
             <h3>Register</h3>
-            <form>
-                <input type="text" placeholder="Username" />
-                <input type="text" placeholder="Email" />
-                <input type="password" placeholder="Password" />
+            <form onSubmit={submit}>
+                <input type="text" value={username} onChange={e => setUsername(e.target.value)} placeholder="Username" />
+                <input type="email" value={email}onChange={e => setEmail(e.target.value)}  placeholder="Email" />
+                <input type="password" onChange={e => setPassword(e.target.value)}  value={password} placeholder="Password" />
                 <button>Sign up</button>
             </form>
         </div>
