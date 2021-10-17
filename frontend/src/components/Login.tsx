d7cb7141eb45b957cbd647c1c084e7aeae79343a
hampus <hampus.john.olausson@gmail.com>
2021-10-17 12:54:05 +0200

    Login user works successfully

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,27 +1,36 @@
-import React, { useState } from 'react'
+import React, { SyntheticEvent, useState } from 'react'
+import { Redirect } from 'react-router';
 
 const Login = () => {
 
-
-    const [userName, setUserName] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('')
+    const [redirect, setRedirect] = useState(false);
 
-//    async function getUserData(){
-//        let response = await fetch({
-//            method: "GET",
-//            url: 'http://localhost:5000/Login',
-//        });
-       
-//        return response.json
-//    }
     
+    const submit = async (e: SyntheticEvent) => {
+        await fetch('http://localhost:8000/api/login', {
+            method: "POST",
+            headers: {'Content-Type': 'Application/json'},
+            body: JSON.stringify({
+                email,
+                password
+            })
+        });
+
+        setRedirect(true);
+    }
+
+    if(redirect){
+        return <Redirect to="/"/>
+    }
 
     return (
         <div>
             <h3>Login</h3>
-            <form>
-            <input type="text" placeholder="email" onChange={e => setUserName(e.target.value)}/>
-            <input type="password" placeholder="password" onChange={e => setPassword(e.target.value)}/>
+            <form onSubmit={submit}>
+            <input type="email" value={email} placeholder="email" onChange={e => setEmail(e.target.value)}/>
+            <input type="password" value={password} placeholder="password" onChange={e => setPassword(e.target.value)}/>
             <button>Sign in</button>
             </form>
         </div>
