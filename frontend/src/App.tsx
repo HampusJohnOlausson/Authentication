73fe0f68528a0e6e0fe21fc44f576aee1ffce852
hampus <hampus.john.olausson@gmail.com>
2021-10-17 13:12:21 +0200

    Logout and deleting cookie works successfully

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import Register from './components/Register';
 import Nav from './components/Nav';
 import Login from './components/Login';
 
 function App() {
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    (
+        async () => {
+            const response = await fetch('http://localhost:8000/api/user', {
+            headers: {'Content-Type': 'Application/json'},
+            credentials: 'include',
+        });
+
+        const content = await response.json();
+
+        setName(content.name);
+        }
+    )();
+});
+
   return (
     <BrowserRouter>
     
     <div className="App">
       <Nav/>
       <Switch>
-        <Route exact path="/" component={Home}/>
+        <Route exact path="/"  component={() => <Home name={name}/>} />
         <Route exact path="/Register" component={Register}/>
         <Route exact path="/Login" component={Login}/>
       </Switch>
--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,7 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Nav = () => {
+const Nav = (props: {name: string}) => {
+
+
+    const logout = async () => {
+        await fetch('http://localhost:8000/api/logout', {
+            method: "POST",
+            headers: {'Content-Type': 'Application/json'},
+            credentials: 'include',
+        });
+    }
+
+    let menu;
+
+    if(props.name === ''){
+        menu = (
+            <div>
+                <Link to="/Register">Register</Link>
+                <Link to="/Login">Login</Link>
+           </div>
+        )
+    } else {
+        menu = (
+            <div>
+                <Link to="/Logout" onClick={logout}>Logout</Link>
+           </div>
+        )
+    }
+
     return (
         <div>
            <Link to="/">Home</Link> 
